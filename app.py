529dfc6b90b13058e52ca940671c2d31732a804f
Your Name <you@example.com>
2025-03-13 14:51:32 +0000

    adding azure.monitor.opentelemetry

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request
-from datetime import datetime, timezone
 
+from datetime import datetime, timezone
 from werkzeug.utils import redirect
 from flask_config import Config
 from data.database import initialise_database, add_order, clear_orders, count_orders, get_orders_to_display, get_queued_count, get_recently_placed_count, get_recently_processed_count
 from scheduled_jobs import initialise_scheduled_jobs
 from products import create_product_download
 import requests
+from azure.monitor.opentelemetry import configure_azure_monitor
+import logging
+
+logging.basicConfig(level=logging.INFO)
+configure_azure_monitor()
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config.from_object(Config)
